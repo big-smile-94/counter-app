73e9a5db81d3f0a999ff068f9618456c828aeae0
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-11 02:48:13 +0000

    set bootstrap class attributes to the counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  //   handleOnClick = (event) => {
+  //     this.setState()
+  //   }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </>
+    );
   }
 }
 
