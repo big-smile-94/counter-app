31ed105b953d07692587fdd9e80df0cb608d3734
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-12 00:28:09 +0000

    lift the state of the counters component to its parent App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
+import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/navbar';
 import Counters from './components/counters';
 
-function App() {
-  return (
-    <>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counter) => {
+    const counters = this.state.counters.filter((c) => c.id !== counter.id);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <>
+        <NavBar />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
+          />
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
